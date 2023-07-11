3d2ddd35fd3001b0257570c4e49958ee516299eb
DESKTOP-SANGMYE\sang <sangtting>
2023-07-12 00:41:10 +0900

    commit

---
+++ src/main/java/com/springboot/catchmind/dto/FavoritesDto.java
@@ -0,0 +1,8 @@
+package com.springboot.catchmind.dto;
+
+import lombok.Data;
+
+@Data
+public class FavoritesDto {
+    private String fid, sid, fmemo, mid, sname, smphoto, lunch, dinner ;
+}
---
+++ src/main/java/com/springboot/catchmind/repository/FavoritesRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.catchmind.repository;
+
+import com.springboot.catchmind.dto.FavoritesDto;
+import org.springframework.web.bind.annotation.Mapping;
+
+
+public interface FavoritesRepository {
+}
