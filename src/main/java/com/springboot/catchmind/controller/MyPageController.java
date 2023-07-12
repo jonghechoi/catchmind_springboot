430cb5aa5165103ec5a36920b97ad207f366c8db
DESKTOP-SANGMYE\sang <sangtting>
2023-07-13 00:39:46 +0900

    commit

--- src/main/java/com/springboot/catchmind/controller/MyPageController.java
+++ src/main/java/com/springboot/catchmind/controller/MyPageController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,12 +37,12 @@ public class MyPageController {
 		SessionVo sessionVo = (SessionVo)session.getAttribute("sessionVo");
 		String mid = sessionVo.getMid();
 		MemberVo memberList = memberDao.nameSelect(mid);
-		
+
 		model.addObject("memberList", memberList);
 		model.addObject("mid", mid);
         model.setViewName("mypage");
-	
-        return model; 
+
+        return model;
 	}
 	
 	/**
