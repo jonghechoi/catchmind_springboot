28c37e001e6af400552097a9a32c33bf34d5171c
Dada-none <gadin8631@naver.com>
2023-08-07 16:53:32 +0900

    application.properties + logback-spring.xml + JPA 기본 CRUD + Trial 테스트 세팅 후 커밋

---
+++ src/main/java/com/springboot/catchmind/trial/entity/TrialRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.catchmind.trial.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrialRepository extends JpaRepository<Trial, Long> {
+
+}
